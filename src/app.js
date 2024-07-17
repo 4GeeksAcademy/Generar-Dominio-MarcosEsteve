e608b939d019f40171600404ea36889cc003d705
MarcosEstPam <marcosepamias@gmail.com>
2024-07-17 17:36:28 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,42 +1,46 @@
-
 window.onload = function() {
-let pronoun = ["the", "our", "his", "her", "its", "their", "my", "your", "our", "their", "a", "an"]
-let adj = ["great", "big", "small", "tiny", "huge", "gigantic", "magnificent", "lovely", "charming", "elegant", "bright", "dark"];
-let noun = ["jogger", "racoon", "lion", "tiger", "bear", "eagle", "shark", "wolf", "dragon", "phoenix", "unicorn", "dolphin"];
+  let pronoun = ["the", "our", "his", "her", "its", "their", "my", "your", "our", "their", "a", "an"];
+  let adj = ["great", "big", "small", "tiny", "huge", "gigantic", "magnificent", "lovely", "charming", "elegant", "bright", "dark"];
+  let noun = ["jogger", "racoon", "lion", "tiger", "bear", "eagle", "shark", "wolf", "dragon", "phoenix", "unicorn", "dolphin"];
 
-function GenerarDominio(primero, segundo, tercero) {
-  let Dominio = "";
-  let contador = 0;
-  for (let i = 0; i < primero.length; i++) {
-    for (let j = 0; j < segundo.length; j++) {
-      for (let x = 0; x < tercero.length; x++) {
-        Dominio += primero[i] + segundo[j] + tercero[x] + ".com\n";
-        contador += 1;
+  let iteraciones = "";
+  const inputElemento = document.querySelector("#inputusuario");
+  inputElemento.addEventListener("input", function(event) {
+      iteraciones = event.target.value;
+      const seleccionaOtro = document.querySelector("#seleccionaotro");
+      if (iteraciones > pronoun.length) {
+          seleccionaOtro.innerHTML = "Introduce otro número";  
+      } else {
+          seleccionaOtro.innerHTML = "Has seleccionado " +iteraciones*3+ " dominios.";  
       }
-    }
-  }
-  return Dominio;
-}
-function cuenta(primero, segundo, tercero) {
-  let contador = 0;
-  for (let i = 0; i < primero.length; i++) {
-    for (let j = 0; j < segundo.length; j++) {
-      for (let x = 0; x < tercero.length; x++) {
-        contador += 1;
+  }); // Cierre correcto del event listener
+
+  function GenerarDominio(primero, segundo, tercero) {
+      let Dominio = "";
+      let contador = 0;
+      for (let i = 0; i < primero.length; i++) {
+          for (let j = 0; j < segundo.length; j++) {
+              for (let x = 0; x < tercero.length; x++) {
+                  Dominio += primero[i] + segundo[j] + tercero[x] + ".com\n";
+                  contador += 1;
+              }
+          }
       }
-    }
+      return Dominio;
   }
-  return contador;
-}
+
+  function cuenta(primero, segundo, tercero) {
+      let contador = 0;
+      for (let i = 0; i < primero.length; i++) {
+          for (let j = 0; j < segundo.length; j++) {
+              for (let x = 0; x < tercero.length; x++) {
+                  contador += 1;
+              }
+          }
+      }
+      return contador;
+  }
+
   document.querySelector("#dominios").innerText = GenerarDominio(pronoun, adj, noun);
   document.querySelector("#conteo").innerText = cuenta(pronoun, adj, noun);
-  
-  let parrafo = document.querySelector("#excusa");
-  parrafo.innerHTML = generar_excusa();
-  document.getElementById("botonexcusa").addEventListener("click", function() {
-    parrafo.innerHTML = generar_excusa();
-  });
-  
 };
-console.log (cuenta());
-
